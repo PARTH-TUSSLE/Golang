26cdef2379e28c9258d399b7ebc37032c7bc9730
PARTH-TUSSLE <parthgartan26feb@gmail.com>
2025-09-21 11:27:14 +0530

    Data Types

---
+++ 2.DataTypes/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	//INTEGER
+	fmt.Println(2+2)
+	// STRINGS
+	fmt.Println("Hello" + " Golang!")
+	//BOOL
+	fmt.Println(true)
+	fmt.Println(false)
+	// FLOAT
+	fmt.Println(7.0/3.0)
+}
