1d759c7aeffe56f621d76453eca201874c1f9d13
PARTH-TUSSLE <parthgartan26feb@gmail.com>
2026-06-03 19:40:17 +0530

    maps

---
+++ 10.Maps/main.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+//maps -> hash tables, objects, dicts
+
+func main() {
+	//creation
+	m := make(map[string]string)
+
+	m["name"] = "Parth"
+	fmt.Println(m["name"], len(m)) // if key DNE -> returns zeroes value of that data type(of the value)
+	
+	delete(m, "name");
+	fmt.Println(m["name"], len(m))
+
+	clear(m) // -> deletes all the elements in the map
+
+	m2 := map[string]int { "price": 200, "age": 12 }
+
+	fmt.Println(m2)
+
+	// element exists or not
+
+	v, ok := m2["pric"]
+	if ok {
+		fmt.Println("All ok!", v)
+	} else {
+		fmt.Println("not ok!", v)
+	}
+
+}
